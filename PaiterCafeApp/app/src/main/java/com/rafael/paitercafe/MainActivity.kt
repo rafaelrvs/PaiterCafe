23b043dc6e0905a67cf852d8417fe7f08f9b521d
rafaelrvs <rafaelvieira1333@gmail.com>
2023-11-14 23:11:59 -0300

    adicionando telas

--- PaiterCafeApp/app/src/main/java/com/rafael/paitercafe/MainActivity.kt
+++ PaiterCafeApp/app/src/main/java/com/rafael/paitercafe/MainActivity.kt
@@ -1,16 +1,38 @@
 package com.rafael.paitercafe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var btnMainREgister : Button
 
 
     private val auth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnMainREgister.findViewById<Button>(R.id.mainbtnCadastro)
+
+btnMainREgister.setOnClickListener{view->
+
+    val login =  f
+
+    val intent = Intent(this,login::class.java)
+    startActivity(intent)
+
+}
+
+
+
+
+
+
 
 
     }
